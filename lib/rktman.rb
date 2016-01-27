061a56297b252a0328847b4f0898130e92cc2a11
Alexander von Gluck IV <kallisti5@unixzen.com>
2016-01-27 10:02:12 -0600

    rktman: Fix version call to be functional

--- lib/rktman.rb
+++ lib/rktman.rb
@@ -3,10 +3,21 @@ require "rktman/version"
 require "api"
 require "api_services"
 
+require "pp"
+
 class RktMan
+  attr_accessor :host, :port
+
+  def initialize
+    # set defaults
+    @host = "localhost"
+    @port = 15441
+  end
+
   def version
-    stub = V1alpha::PublicAPI::Stub.new('localhost:15441', :this_channel_is_insecure)
-    info = V1alpha::Info.new()
-    stub.get_info(info)
+    stub = V1alpha::PublicAPI::Stub.new("#{@host}:#{@port}", :this_channel_is_insecure)
+    request = V1alpha::Info.new()
+    response = stub.get_info(request)
+    response.info['rkt_version']
   end
 end
--- test/init_test.rb
+++ test/init_test.rb
@@ -6,4 +6,14 @@ class BaseTest < Minitest::Test
   def test_init
     refute_nil RktMan.new
   end
+
+  def test_default_host
+    rkt = RktMan.new
+    refute_nil rkt.host
+  end
+
+  def test_default_port
+    rkt = RktMan.new
+    refute_nil rkt.port
+  end
 end
